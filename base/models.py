bc8cbddbe80a26cb28bf34d3db5cf9be6a129785
noulmon <noulmonkc@gmail.com>
2022-11-09 01:07:48 +0530

    Added base app models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+# base date model
+class DatedModel(models.Model):
+    class Meta:
+        abstract = True
+
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+
+# base status model
+class StatusModel(models.Model):
+    class Meta:
+        abstract = True
+
+    is_active = models.BooleanField(default=True)
