7cb61a429ff89715f0dc302f06a7f6476d13b630
noulmon <noulmonkc@gmail.com>
2022-11-09 02:18:52 +0530

    Added work percentage validator

--- work/models.py
+++ work/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from base.models import DatedModel, StatusModel
 from employee.models import Employee
+from django.core.validators import MaxValueValidator
 
 
 # Work Arrangement Model
@@ -22,7 +23,7 @@ class WorkArrangement(DatedModel, StatusModel):
 class EmployeeWorkArrangement(DatedModel, StatusModel):
     employee = models.ForeignKey(Employee, null=False, on_delete=models.CASCADE)
     work_arrangement = models.ForeignKey(WorkArrangement, null=False, on_delete=models.CASCADE)
-    percentage = models.DecimalField(null=False, max_digits=10, decimal_places=2)
+    percentage = models.DecimalField(null=False, max_digits=10, decimal_places=2, validators=[MaxValueValidator(100)])
 
     def __str__(self):
         return f'{self.employee.employee_id} - {self.work_arrangement.work} ({self.percentage})'
