0ac92e6206f0cb16ee194514df828a527eb8073b
noulmon <noulmonkc@gmail.com>
2022-11-10 01:34:10 +0530

    Added employee views testcases

---
+++ employee/tests/test_employee_views.py
@@ -0,0 +1,81 @@
+import json
+from decimal import Decimal
+
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from employee.models import Employee, Team
+
+
+class EmployeeViewTests(APITestCase):
+
+    def setUp(self):
+        self.list_create_url = reverse('employee-list-create')
+        self.employee = Employee.objects.create(employee_name="Hugo", hourly_rate=Decimal(200))
+        self.team = Team.objects.create(name="ENGINEERING")
+
+    def test_get_employee_list(self):
+        response = self.client.get(self.list_create_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_duplicate_employee_creation(self):
+        data = {
+            "employee_name": "Hugo",
+            "hourly_rate": 250,
+        }
+        response = self.client.post(self.list_create_url, format='json', data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_employee(self):
+        data = {
+            "employee_name": "John",
+            "hourly_rate": 200,
+        }
+        data = json.dumps(data)
+        response = self.client.post(self.list_create_url, content_type='application/json', data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['employee_id'], 'EDI-EMP-2')
+
+    def test_invalid_team_id(self):
+        data = {
+            "employee_name": "John",
+            "hourly_rate": 200,
+            "team": 2
+        }
+        data = json.dumps(data)
+        response = self.client.post(self.list_create_url, content_type='application/json', data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_employee_detail_view(self):
+        response = self.client.get(reverse('employee-retrieve-update-delete', kwargs={'pk': self.employee.id}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['employee_name'], 'Hugo')
+
+    def test_get_invalid_employee_id(self):
+        response = self.client.get(reverse('employee-retrieve-update-delete', kwargs={'pk': 100}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_employee_team(self):
+        team_id = self.team.id
+        data = {
+            'team': team_id
+        }
+        data = json.dumps(data)
+        url = reverse('employee-retrieve-update-delete', kwargs={'pk': self.employee.id})
+        response = self.client.patch(url, content_type='application/json', data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.employee.id, team_id)
+
+    def test_update_duplicate_employee_data(self):
+        new_team = Team.objects.create(name="DATA")
+        new_employee = Employee.objects.create(employee_name="Rohit", hourly_rate=Decimal(100))
+
+        data = {
+            'employee_name': "Rohit",
+            'team': new_team.id
+        }
+        data = json.dumps(data)
+        url = reverse('employee-retrieve-update-delete', kwargs={'pk': self.employee.id})
+        response = self.client.patch(url, content_type='application/json', data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
--- employee/urls.py
+++ employee/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from employee import views
 
 urlpatterns = [
-    path('api/employees/', views.EmployeeList.as_view()),
-    path('api/employee/<int:pk>/', views.EmployeeDetail.as_view()),
+    path('api/employees/', views.EmployeeList.as_view(), name='employee-list-create'),
+    path('api/employee/<int:pk>/', views.EmployeeDetail.as_view(), name='employee-retrieve-update-delete'),
 
 ]
