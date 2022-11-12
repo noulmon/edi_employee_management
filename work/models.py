19fe86c42a03a44853a27a0b63698294bde19250
noulmon <noulmonkc@gmail.com>
2022-11-12 21:09:26 +0530

    Added level validation in EmployeeWorkArrangement model

--- work/models.py
+++ work/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MaxValueValidator, ValidationError
 from django.db import models
+from django.db.models import Sum
 
 from base.models import DatedModel, StatusModel
 from employee.models import Employee
@@ -34,6 +35,21 @@ class EmployeeWorkArrangement(DatedModel, StatusModel):
     employee = models.ForeignKey(Employee, null=False, on_delete=models.CASCADE)
     work_arrangement = models.ForeignKey(WorkArrangement, null=False, on_delete=models.CASCADE)
     percentage = models.DecimalField(null=False, max_digits=10, decimal_places=2, validators=[MaxValueValidator(100)])
+
+    def get_total_employee_work_percentage(self):
+        """
+        Returns total work arrangement percentage of an employee
+        """
+        return self.employee.employeeworkarrangement_set.all().aggregate(Sum("percentage"))['percentage__sum']
+
+    def save(self, *args, **kwargs):
+        if self.percentage > 100:
+            raise ValidationError('Error: Percentage should not be greater than 100')
+
+        if EmployeeWorkArrangement.objects.filter(employee=self.employee).exists():
+            if self.get_total_employee_work_percentage() + self.percentage > 100:
+                raise ValidationError('Error: No employee can have total work percentage greater than 100')
+        super(EmployeeWorkArrangement, self).save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.employee.employee_id} - {self.work_arrangement.work} ({self.percentage})'
