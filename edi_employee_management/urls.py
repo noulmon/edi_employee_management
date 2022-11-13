c1e7c7c4521d5fd09254f375c29431c354711734
noulmon <noulmonkc@gmail.com>
2022-11-14 02:16:15 +0530

    Added drf-yasg (swagger) for API documentation

--- edi_employee_management/urls.py
+++ edi_employee_management/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from edi_employee_management.settings import schema_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("employee/", include("employee.urls")),
     path("work/", include("work.urls")),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
 ]
