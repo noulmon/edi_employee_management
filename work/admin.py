4a7418c5f76ead4363008c74d18e756a8274a029
noulmon <noulmonkc@gmail.com>
2022-11-14 13:21:56 +0530

    Registered models in admin panel

--- work/admin.py
+++ work/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import WorkArrangement, EmployeeWorkArrangement
+
 # Register your models here.
+admin.site.register(WorkArrangement)
+admin.site.register(EmployeeWorkArrangement)
