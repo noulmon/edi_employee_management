c4de3e7496e1fcba0ecb0178896b5d19ccbd3a53
noulmon <noulmonkc@gmail.com>
2022-11-14 17:52:49 +0530

    Refactored work serializer

--- work/serializers.py
+++ work/serializers.py
@@ -10,8 +10,9 @@ class WorkArrangementSerializer(serializers.ModelSerializer):
 
 
 def validate_total_work_percentage(employee, percentage):
-    per = employee.get_total_employee_work_percentage()
-    if per + percentage > 100:
+    """returns true if the total assigned work percentage of an employee is lees than 100, else false"""
+    total_percentage = employee.get_total_employee_work_percentage()
+    if total_percentage + percentage > 100:
         return False
     return True
 
@@ -22,13 +23,16 @@ class EmployeeWorkArrangementSerializer(serializers.ModelSerializer):
         exclude = ['date_added', 'date_modified']
 
     def create(self, validated_data):
+        """creates an EmployeeWorkArrangement object"""
         employee = validated_data['employee']
         percentage = validated_data['percentage']
         if not validate_total_work_percentage(employee, percentage):
+            # validates if the total employee work percentage is less than 100
             raise serializers.ValidationError('Error: No employee can have total work percentage greater than 100')
         return self.Meta.model.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
+        """updates an existing EmployeeWorkArrangement instance"""
         percentage = validated_data.get('percentage')
         employee = instance.employee
         # if the update data includes the employee
@@ -36,6 +40,7 @@ class EmployeeWorkArrangementSerializer(serializers.ModelSerializer):
         if payload_employee:
             employee = payload_employee
         if percentage:
+            # validates if the total employee work percentage is less than 100
             if not validate_total_work_percentage(employee, percentage):
                 raise serializers.ValidationError('Error: No employee can have total work percentage greater than 100')
         for key, value in validated_data.items():
